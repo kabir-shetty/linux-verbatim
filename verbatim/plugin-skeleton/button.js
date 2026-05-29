2d940f9a6a0000209d0cf9d523988a08f7a00540
kabir-shetty <kabir.n.shetty@gmail.com>
2026-05-29 17:41:32 -0500

    Added debug text to button.js to check if getToolbarItems runs and changed config.json isVisual and isInsideMode to true

--- verbatim/plugin-skeleton/button.js
+++ verbatim/plugin-skeleton/button.js
@@ -1,18 +1,18 @@
-window.Asc.plugin.executeMethod("InputText", ["button.js loaded"]);
-
-function getToolbarItems() {
-    let items = {
-      guid: window.Asc.plugin.info.guid,
-      tabs: [{
-        id: "tab_1",
-        text: "Insert options",
-        items: []
-      }]
+window.Asc.plugin.init = function () {
+    function getToolbarItems() {
+        window.Asc.plugin.executeMethod("InputText", ["geToolbarItems ran"]);
+        let items = {
+          guid: window.Asc.plugin.info.guid,
+          tabs: [{
+            id: "tab_1",
+            text: "Insert options",
+            items: []
+          }]
+        };
+    
+        return items;
     };
 
-    return items;
-};
-
-window.Asc.plugin.init = function () {
+    window.Asc.plugin.executeMethod("InputText", ["button.js loaded"]);
     this.executeMethod("AddToolbarMenuItem", [getToolbarItems()]);
 }
